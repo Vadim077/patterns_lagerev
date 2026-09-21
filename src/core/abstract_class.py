3ac74d4260b62f1c57adaffe326235d7305ab1d3
Vadim077 <lagvadim@gmail.com>
2026-09-21 13:19:33 +0800

    Edit abstract_model

--- src/core/abstract_class.py
+++ src/core/abstract_class.py
@@ -2,21 +2,26 @@ from abc import ABC
 import uuid
 
 class name_id(ABC):
+    """Базовый абстрактный класс с id и именем."""
 
     def __init__(self):
+        """Конструктор."""
         self.__name = ""
         self.__id = uuid.uuid4()
 
     @property
     def id(self):
+        """Получить id."""
         return self.__id
 
     @property
     def name(self) -> str:
+        """Получить имя."""
         return self.__name
 
     @name.setter
     def name(self, new_name: str):
+        """Установить имя."""
         if new_name is not None and len(new_name) > 0:
             self.__name = new_name
         else:
